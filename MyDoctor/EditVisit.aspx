8d60fb07684297235d2ddf22bcd7dec4155b0b9e
kurs <kurs@DESKTOP-V3HDKPQ>
2021-09-05 16:38:21 +0200

    final

---
+++ MyDoctor/EditVisit.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EditVisit.aspx.cs" Inherits="MyDoctor.EditVisit" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h3>Zmiana statusu wizyty</h3>
+    <table class="table">
+
+        <tr>
+            <td>Zmiana statusu                
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlStatus" runat="server">
+                    <asp:ListItem Value="0">---</asp:ListItem>
+                    <asp:ListItem Value="1">AKCEPTACJA</asp:ListItem>
+                    <asp:ListItem Value="-1">ANULOWANO</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <asp:Label ID="lblResult" ForeColor="Red" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <asp:HiddenField ID="tbHiddenId" runat="server" />
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnOK" runat="server" Text="Zapisz" OnClick="btnOK_Click" /></td>
+        </tr>
+    </table>
+
+</asp:Content>
---
+++ MyDoctor/EditVisit.aspx.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyDoctor
+{
+    public partial class EditVisit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int recordId = Convert.ToInt32(Request.Params["id"]);
+                tbHiddenId.Value = recordId.ToString();
+            } catch (Exception exc)
+            {
+                Response.Redirect("~/VisitList");
+            }
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            int status = Convert.ToInt32(ddlStatus.SelectedValue);
+            if (status == 0)
+            {
+                lblResult.Text = "Wybierz odpowiedni status";
+            }
+            else
+            {
+                String sql = $"UPDATE visits SET status={status} WHERE id={Convert.ToInt32(tbHiddenId.Value)}";
+                String cs =
+                ConfigurationManager.ConnectionStrings["edoctorConnectionString"].ConnectionString;
+                using (MySqlConnection conn = new MySqlConnection(cs))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    Response.Redirect("~/VisitList");
+                }
+            }
+        }
+    }
+}
